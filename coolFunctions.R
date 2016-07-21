d77b5587e89c9ffede309e50f4333cf42102335a
MHaneferd <mhaneferd@gmail.com>
2016-07-22 00:32:29 +0200

    Lagt inn siste endring fra kurset

--- coolFunctions.R
+++ coolFunctions.R
@@ -11,3 +11,12 @@ above <- function(x, y=10){
   use <- x > y
   x[use]
 }
+
+columnmean <- function(x, removeNA = TRUE){
+     nc <- ncol(x)
+     means = numeric(nc)
+    for(i in nc) {
+         means[i] <- mean(x[,i], na.rm = removeNA)
+    }
+    means
+}
