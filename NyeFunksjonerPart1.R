e04002051c09aa5761f54ecde5af3bc66cf4c4f6
MHaneferd <mhaneferd@gmail.com>
2016-07-22 01:07:47 +0200

    Formatert koden i coolFunctions. Testet kommentar i NyeFunksjoner

--- NyeFunksjonerPart1.R
+++ NyeFunksjonerPart1.R
@@ -1,1 +1,11 @@
-z <- 10
+#' Standardavik
+#'
+#' @param en tallrekke (vektor med tall) 
+#'
+#' @return Standardavviket til tallrekken
+#' @export
+#'
+#' @examples
+standardavik <- function(x){
+  sd(x)
+}
--- coolFunctions.R
+++ coolFunctions.R
@@ -1,22 +1,22 @@
-add2 <- function(x,y){
-  x+y
+add2 <- function(x, y) {
+  x + y
 }
 
-above10 <- function(x){
+above10 <- function(x) {
   use <- x > 10
   x[use]
 }
 
-above <- function(x, y=10){
+above <- function(x, y = 10) {
   use <- x > y
   x[use]
 }
 
-columnmean <- function(x, removeNA = TRUE){
-     nc <- ncol(x)
-     means = numeric(nc)
-    for(i in nc) {
-         means[i] <- mean(x[,i], na.rm = removeNA)
-    }
-    means
+columnmean <- function(x, removeNA = TRUE) {
+  nc <- ncol(x)
+  means = numeric(nc)
+  for (i in nc) {
+    means[i] <- mean(x[, i], na.rm = removeNA)
+  }
+  means
 }
